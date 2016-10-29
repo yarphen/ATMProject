9b153ffc33a54b4877d33743aaf7e0a03d547eb7
AnatoliyMovchan <AnatoliyMovchan@yahoo.com>
2016-10-29 11:00:28 +0300

    Exception templates

---
+++ ATMClient/Exceptions.h
@@ -0,0 +1,21 @@
+class CannotConnect{
+
+};
+class ErrorOnReceive{
+
+};
+class NoCacheLeft{
+
+};
+class NotEnoughMoney{
+
+};
+class InvalidCard{
+
+};
+class InvalidReceiver{
+
+};
+class WrongPIN{
+
+};
