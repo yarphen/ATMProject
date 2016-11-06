b42c7e686ed0d037ae6189a8ca79210ce2d0a7f9
AnatoliyMovchan <anatoliymovchan@yahoo.com>
2016-11-06 16:00:41 +0200

    Creating sample client

---
+++ ATMClient/BankSecureClient.cpp
@@ -0,0 +1,115 @@
+#include "BankSecureClient.h"
+#include "Exceptions.h"
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+
+#define DEFAULT_BUFLEN 4096
+#define HOST "127.0.0.1"
+#define DEFAULT_PORT "27015"
+
+char * BankSecureClient::process(char * message){
+	WSADATA wsaData;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	struct addrinfo *result = NULL,
+		*ptr = NULL,
+		hints;
+	char *sendbuf = "this is a test";
+	char recvbuf[DEFAULT_BUFLEN];
+	int iResult;
+	int recvbuflen = DEFAULT_BUFLEN;
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed with error: %d\n", iResult);
+		throw new CannotConnect();
+	}
+
+	ZeroMemory( &hints, sizeof(hints) );
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	// Resolve the server address and port
+	iResult = getaddrinfo(HOST, DEFAULT_PORT, &hints, &result);
+	if ( iResult != 0 ) {
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		throw new CannotConnect();
+	}
+
+	// Attempt to connect to an address until one succeeds
+	for(ptr=result; ptr != NULL ;ptr=ptr->ai_next) {
+
+		// Create a SOCKET for connecting to server
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, 
+			ptr->ai_protocol);
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("socket failed with error: %ld\n", WSAGetLastError());
+			WSACleanup();
+			throw new CannotConnect();
+		}
+
+		// Connect to server.
+		iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+		if (iResult == SOCKET_ERROR) {
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
+		WSACleanup();
+		throw new CannotConnect();
+	}
+
+	// Send an initial buffer
+	iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf), 0 );
+	if (iResult == SOCKET_ERROR) {
+		printf("send failed with error: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		throw new CannotConnect();
+	}
+
+	printf("Bytes Sent: %ld\n", iResult);
+
+	// shutdown the connection since no more data will be sent
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		throw new CannotConnect();
+	}
+
+	// Receive until the peer closes the connection
+
+	iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+	if ( iResult > 0 && iResult < DEFAULT_BUFLEN){
+		recvbuf[iResult] = 0;
+		printf("Bytes received: %d\n", iResult);
+	} else {
+		throw new ErrorOnReceive();
+	}
+	// cleanup
+	closesocket(ConnectSocket);
+	WSACleanup();
+	return &recvbuf[0];
+}
